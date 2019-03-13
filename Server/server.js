e1785e284cbe67fc80cc32150a6019c9d84a831d
unknown <giulianocrivelli@hotmail.com>
2019-03-13 18:02:17 -0400

    Adicionando descrição

--- Server/server.js
+++ Server/server.js
@@ -12,7 +12,7 @@ mongoose
   .connect(
     //'mongodb://nodeapi:nodeapi123@ds331135.mlab.com:31135/node-api',
     // 'mongodb+srv://<USUARIO>:<SENHA>@cluster0-plxve.mongodb.net/node-api?retryWrites=true',
-     'mongodb+srv://giuliano:batata@cluster0-15dj6.mongodb.net/node-api?retryWrites=true',
+     'mongodb+srv://giuliano:batata@cluster0-15dj6.mongodb.net/GerenciadorDeContas?retryWrites=true',
     {
       useNewUrlParser: true
     })
